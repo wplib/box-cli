006d448dc9ddb5a853a9c54c04c260df43ad6996
Mike Schinkel <mike@newclarity.net>
2018-07-30 01:16:19 -0400

    Restored example. Uploaded `box` command file

--- executables/dhub/main.go
+++ executables/dhub/main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"github.com/wplib/go-lib/docker/auth"
-	"github.com/wplib/go-lib/docker/image"
+"github.com/wplib/go-lib/docker/auth"
+"github.com/wplib/go-lib/docker/image"
+"fmt"
 )
 const img = "wplib/nginx"
 func main() {
@@ -10,9 +11,23 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
+	tags,err := image.GetRemoteImageTags(t,img)
+	if err != nil {
+		panic(err)
+	}
+	for _,t := range tags {
+		if ( t=="latest") {
+			continue
+		}
+		fmt.Printf("%v\n",t)
+	}
+
 	tt,err := image.GetRemoteImageTagTree(t,img)
 	if err != nil {
 		panic(err)
 	}
 	tt.Print()
+
 }
+
